98ec08b45519754fccad36d9c7b916a6638cf294
UlviNuri <102859099+UlviNuri@users.noreply.github.com>
2022-11-01 22:15:24 +0400

    javascript

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,25 +1,40 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", (e) => {
+  e.stopPropagation();
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
-
 document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
+  n.addEventListener("click", (e) => {
+    e.stopPropagation();
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
   })
 );
 
+window.addEventListener("click", (e) => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+});
 // const content = document.getElementsByTagName("body")[0];
 // const darkmode = document.getElementById("dark_change");
-// if(darkmode){
+// if (darkmode) {
 //   darkmode.addEventListener("click", () => {
+//     console.log(darkmode);
 //     darkmode.classList.toggle("active");
 //     content.classList.toggle("night");
 //   });
 // }
+window.addEventListener("scroll", () => {
+  var header = document.querySelector("nav");
+  if (window.scrollY >= 250) {
+    header.classList.add("stiky");
+  } else {
+    header.classList.remove("stiky");
+  }
+});
+
 try {
   var swiper = new Swiper(".mySwiper", {
     pagination: {
@@ -52,3 +67,54 @@ try {
 } catch (eror) {
   console.log("Javascript version updated...");
 }
+
+
+
+
+
+const toastBtn=document.getElementById("js_tag")
+toastBtn.addEventListener("click",()=>{
+  Toastify({
+    text: "Sorgunuz Ugurla Qebul Olundu!!!",
+    duration: 3000,
+    destination: "",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
+  
+})
+
+const payment=document.getElementById("odenis")
+payment.addEventListener("click", ()=>{
+  // Toastify({
+  //   text: "Sorgunuz Ugurla Qebul Olundu",
+  //   duration: 3000,
+  //   destination: "",
+  //   newWindow: true,
+  //   close: true,
+  //   gravity: "top", // `top` or `bottom`
+  //   position: "right", // `left`, `center` or `right`
+  //   stopOnFocus: true, // Prevents dismissing of toast on hover
+  //   style: {
+  //     background: "linear-gradient(to right, #00b09b, #96c93d)",
+  //   },
+  //   onClick: function(){} // Callback after click
+  // }).showToast();
+
+  console.log("pokk");
+})
+
+
+
+AOS.init({
+  delay: 1000, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: "ease",
+});
